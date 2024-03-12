09cdf0dbab76828a7e9027441be72c87eb243fca
RomanMrls <162963866+RomanMrls@users.noreply.github.com>
2024-03-12 09:48:32 +0100

    Add files via upload

--- BomberMan.py
+++ BomberMan.py
@@ -14,15 +14,15 @@ game_size = (750,750)
 game_window_pos = pygame.Vector2((screenx-game_size[0])/2,(screeny-game_size[1])/1.5)
 game_window= pygame.Rect(game_window_pos,game_size)
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
-player_size = (50,50)
+player_pos = pygame.Vector2(game_window_pos)
+player_size = (40,40)
 player = pygame.Rect(player_pos,player_size)
 direction = {pygame.K_LEFT: (-5, 0), pygame.K_RIGHT: (5, 0), pygame.K_UP: (0, -5), pygame.K_DOWN: (0, 5)}
 
-obstacle_pos_list = [[(68.75*i+game_window_pos.x,68.75*j+game_window_pos.y) for i in range(1,11,2)] for j in range(1,11,2)]
-obstacle_size = (68.75,68.75)
-obstacle_rect_list = [[pygame.Rect(pos,obstacle_size) for pos in ligne] for ligne in obstacle_pos_list]
+obstacle_pos_list = [[(56.25*i+game_window_pos.x,56.25*j+game_window_pos.y) for i in range(1,13,2)] for j in range(1,13,2)]
+obstacle_size = (56.25,56.25)
+obstacle_list = [[pygame.Rect(pos,obstacle_size) for pos in ligne] for ligne in obstacle_pos_list]
+obstacle_list = sum(obstacle_list, [])
 
 clock = pygame.time.Clock()
 
@@ -35,21 +35,20 @@ while not game_over:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    if game_window.contains(player):
-        pygame.draw.rect(background,"green",player)
-        if event.type == pygame.KEYDOWN:
-            if event.key in direction:
-                v = direction[event.key]
+            
+    pygame.draw.rect(background,"green",player)
+    if event.type == pygame.KEYDOWN:
+        if event.key in direction:
+            v = direction[event.key]
+            temp_player = player.copy()
+            temp_player.move_ip(v)
+            if game_window.contains(temp_player) and temp_player.collidelistall(obstacle_list) == list():
                 player.move_ip(v)
-    else:
-        player.clamp_ip(game_window)
-        pygame.draw.rect(background,"green",player)
         
     screen.blit(background,(0, 0))
     pygame.draw.rect(background,"blue",game_window)
-    for ligne in obstacle_rect_list:
-        for rect in ligne:
-            pygame.draw.rect(background,"red",rect)
+    for rect in obstacle_list:
+        pygame.draw.rect(background,"red",rect)
     pygame.display.update()
     
     dt = clock.tick(60) // 2000
