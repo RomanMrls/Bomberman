32e2c9b40b345fe93f4cfd9e3c0ba3265e93bee2
RomanMrls <162963866+RomanMrls@users.noreply.github.com>
2024-03-13 19:52:36 +0100

    Add files via upload
    ajout de generation des briques et clé + passage a l'etage d'apres

--- BomberMan.py
+++ BomberMan.py
@@ -1,4 +1,8 @@
-import pygame,sys,math
+import pygame
+import sys
+import math
+import random
+from random import randint
 
 # Initialisation de Pygame
 pygame.init()
@@ -41,18 +45,20 @@ bomb_size = (25, 25)
 def bomb_pos():
     player_center = pygame.Vector2(player.center)
     player_relative_pos = player_center - pygame.Vector2(game_window.topleft)
-    player_relative_grid_pos = player_relative_pos.elementwise() // pygame.Vector2(obstacle_size)
-    grid_square_center = player_relative_grid_pos.elementwise() * pygame.Vector2(obstacle_size) + pygame.Vector2(obstacle_size) / 2
+    player_relative_grid_pos = player_relative_pos.elementwise() // pygame.Vector2(unbreakables_size)
+    grid_square_center = player_relative_grid_pos.elementwise() * pygame.Vector2(unbreakables_size) + pygame.Vector2(unbreakables_size) / 2
     bomb_relative_pos = grid_square_center + pygame.Vector2(game_window.topleft) - pygame.Vector2(bomb_size) / 2
     return bomb_relative_pos
 
-# Liste des positions des obstacles et initialisation de la taille des obstacles
-obstacle_pos_list = [[(game_size[0] / 13 * i + game_window_pos.x, game_size[1] / 13 * j + game_window_pos.y) for i in range(1, 13, 2)] for j in range(1, 13, 2)]
-obstacle_size = (game_size[0] / 13, game_size[1] / 13)
-# Création des rectangles des obstacles
-obstacle_list = [[pygame.Rect(pos, obstacle_size) for pos in ligne] for ligne in obstacle_pos_list]
-# Aplatir la liste d'obstacles en une seule liste
-obstacle_list = sum(obstacle_list, [])
+# Liste des positions des briques unbreakables et initialisation de la taille des briques unbreakables
+unbreakables_size = (game_size[0] / 13, game_size[1] / 13)
+unbreakables_pos_list = [[(round(game_size[0] / 13 * i + game_window_pos.x , 5), round(game_size[1] / 13 * j + game_window_pos.y, 5)) for i in range(1, 13, 2)] for j in range(1, 13, 2)]
+unbreakables_pos_list = sum(unbreakables_pos_list, [])
+
+# Création des rectangles des unbreakabless
+unbreakables_list = [pygame.Rect(pos, unbreakables_size) for pos in unbreakables_pos_list]
+# Aplatir la liste d'unbreakabless en une seule liste
+
 
 # Initialisation de l'horloge pour la gestion du temps
 clock = pygame.time.Clock()
@@ -68,7 +74,7 @@ score_number = 10  # Score initial
 floor_number = 1  # Numéro de l'étage initial
 
 # Dictionnaire pour stocker l'état des touches du clavier
-key_state = {}
+key_pressed_state = {}
 
 # Initialisation de la variable de temps écoulé (delta time)
 dt = 0
@@ -89,6 +95,57 @@ def timer_str(timer_counter):
             timer_str = '0' + str(timer_secondes)
     return timer_str
 
+# Classe Brick pour représenter les blocs cassables
+class Brick:
+    def __init__(self, pos, durability):
+        self.__rect = pygame.Rect(pos, (game_size[0] / 13, game_size[1] / 13))
+        self.__durability = durability
+    
+    def rect(self):
+        return self.__rect
+
+    def boom(self):
+        self.__durability -= 1
+
+    def is_destroyed(self):
+        return self.__durability <= 0
+    
+# Fonction pour générer les briques
+def gen_bricks(num_bricks, player_pos, floor_key):
+    brick_size = (55, 55)
+    bricks_list = []
+
+    for i in range(0, 13):
+        for j in range(0, 13):
+            brick_pos = (round(game_size[0] / 13 * i + game_window_pos.x, 5), round(game_size[1] / 13 * j + game_window_pos.y, 5))
+            brick_rect = pygame.Rect(brick_pos, brick_size)
+            if not brick_rect.contains(floor_key) and brick_rect.collidelistall(unbreakables_list) == []:
+                bricks_list.append(brick_rect)
+    
+    player_temp_rect = pygame.Rect(player_pos, (brick_size[0]*2, brick_size[1]*2))
+    bricks_list = [brick_rect for brick_rect in bricks_list if not player_temp_rect.colliderect(brick_rect)]
+
+    return random.sample(bricks_list, min(num_bricks, len(bricks_list)))
+
+# Fonction pour générer la structure de l'étage
+def gen_floor(floor_number,player_pos):
+    # Génération de la clé
+    key_grid_pos = (randint(0, 6) * 2, randint(0, 6) * 2)
+    key_size = (25,25)
+    
+    key_x = game_window_pos.x + (key_grid_pos[0] * game_size[0] / 13) + (game_size[0] / 13 - key_size[0]) / 2
+    key_y = game_window_pos.y + (key_grid_pos[1] * game_size[1] / 13) + (game_size[1] / 13 - key_size[1]) / 2
+    
+    floor_key = pygame.Rect((key_x, key_y),key_size)
+    
+    #Génération des blocs
+    bricks_list = gen_bricks(100, player_pos, floor_key)
+
+    return floor_key, bricks_list
+
+# Génération du premier étage
+floor_key, bricks_list = gen_floor(floor_number,player_pos)
+
 # Boucle principale du jeu
 while not game_over:
     for event in pygame.event.get():
@@ -106,14 +163,14 @@ while not game_over:
                 bomb_timer = 0
                 bomb_placed_time = pygame.time.get_ticks()
             elif event.key in direction:
-                key_state[event.key] = True 
+                key_pressed_state[event.key] = True
         elif event.type == pygame.KEYUP:
-            key_state[event.key] = False
-    
+            key_pressed_state[event.key] = False
+
     # Calcul de la vélocité du joueur en fonction des touches enfoncées
     player_speed = 200
     player_velocity = pygame.Vector2(0, 0)
-    for key, state in key_state.items():
+    for key, state in key_pressed_state.items():
         if state and key in direction:
             if key in [pygame.K_LEFT, pygame.K_RIGHT]:
                 player_velocity.x += direction[key][0] * player_speed * dt
@@ -122,29 +179,34 @@ while not game_over:
 
     # Déplacement horizontal du joueur
     temp_player = player.move(player_velocity.x, 0)
-    if game_window.contains(temp_player) and temp_player.collidelistall(obstacle_list) == []:
+    if game_window.contains(temp_player) and temp_player.collidelistall(unbreakables_list) == []:
         player.x = temp_player.x
-    
+
     # Déplacement vertical du joueur
     temp_player = player.move(0, player_velocity.y)
-    if game_window.contains(temp_player) and temp_player.collidelistall(obstacle_list) == []:
+    if game_window.contains(temp_player) and temp_player.collidelistall(unbreakables_list) == []:
         player.y = temp_player.y
-    
+
     # Gestion du minuteur de la bombe
     if bomb_timer >= 0:
         bomb_timer += dt
-    
+
     if bomb_timer >= 3:
         bomb = None
         bomb_timer = -1
+    
+    # Génération de l'étage si le joueur a pris la clé
+    if player.colliderect(floor_key):
+        floor_number += 1
+        floor_key, bricks_list = gen_floor(floor_number,player.topleft)
         
     # Rendu du texte de score, du minuteur et du numéro de l'étage
     score = police.render((str(score_number)), True, (255, 255, 255))
     score_center = pygame.Vector2(score.get_rect().center)
-    
+
     timer = police.render((timer_str(timer_counter)), True, (255, 255, 255))
     timer_center = pygame.Vector2(timer.get_rect().center)
-    
+
     floor = police.render((f"Floor : {floor_number}"), True, (255, 255, 255))
     floor_center = pygame.Vector2(floor.get_rect().center)
 
@@ -155,15 +217,18 @@ while not game_over:
     screen.blit(timer, (25, 75 / 2 - timer_center.y))
     screen.blit(floor, (screenx - 25 - floor_center.x * 2 , 75 / 2 - floor_center.y))
     pygame.draw.rect(background, "blue", game_window)
-    for rect in obstacle_list:
-        pygame.draw.rect(background, "red", rect)
+    for rect in unbreakables_list:
+        pygame.draw.rect(background, "purple", rect)
+    for brick in bricks_list:
+        pygame.draw.rect(background, "white", brick)
+    pygame.draw.rect(background, "yellow", floor_key)
     pygame.draw.rect(background, "green", player)
-    
+
     if bomb:
         pygame.draw.rect(background, "black", bomb)
-    
+
     pygame.display.update()
-    
+
     # Calcul du temps écoulé entre deux boucles
     dt = clock.tick(60) / 1000
 
